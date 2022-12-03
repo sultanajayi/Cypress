65089fdec5e7d5943f187fc26e2a1c03d7217ade
sultanajayi <hayobamidele39@gmail.com>
2022-12-03 21:35:54 +0100

    'new'

--- CypressAuto/cypress/integration/projects/test.js
+++ CypressAuto/cypress/integration/projects/test.js
@@ -9,9 +9,11 @@ describe('My First Test Suite', function(){
     cy.get('.search-keyword').type('ca')
     cy.wait(2000)
     cy.get('.product:visible').should('have.length', 4)
-    
-    
-    
+    cy.get('.products').find('.product').should('have.length', 4)
+    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click()
+   
+
+
     });
     
     
