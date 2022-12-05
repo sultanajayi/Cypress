1f754e40e23ffa2fea0a08ca85c85ca2fbe0c18c
sultanajayi <hayobamidele39@gmail.com>
2022-12-05 13:27:11 +0100

    'new'

---
+++ CypressAuto/cypress/integration/projects/test3.js
@@ -0,0 +1,35 @@
+///<reference types="cypress" />
+
+
+describe('My First Test Suite', function(){
+
+    it('My First Test Case', () => {
+        
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    
+   
+   cy.get('#alertbtn').click()
+   cy.get('#confirmbtn').click()
+   cy.on('window:alert', (str)=>{
+
+    expect(str).to.equal('Hello , share this practice page and share your knowledge')
+
+   })
+
+   cy.on('window:confirm', (str)=>{
+
+    expect(str).to.equal('Hello , Are you sure you want to confirm?')
+
+   })
+
+   
+
+
+    
+});
+    
+    
+    
+    
+    })
+
