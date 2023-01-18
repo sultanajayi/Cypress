3e0d2bc3c062a6a217a3737ce1ce7c29dde6654b
sultanajayi <hayobamidele39@gmail.com>
2023-01-18 19:39:33 +0100

    BDD Framework(Cucumber)

--- CypressAuto/cypress.config.js
+++ CypressAuto/cypress.config.js
@@ -1,5 +1,13 @@
 const { defineConfig } = require("cypress");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
 
+async function setupNodeEvents(on, config) {
+  // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+  await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+  on("file:preprocessor", browserify.default(config));
+}
 module.exports = defineConfig({
   defaultCommandTimeout: 8000,
   env: {
@@ -9,7 +17,8 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      setupNodeEvents
     },
-    specPattern: 'cypress/integration/projects/*.js'
+    specPattern: 'cypress/integration/projects/BDD/*.feature'
   },
 });
